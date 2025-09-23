02b23e6b2b7084744eb4be6aab479c6710775d29
LucasZhu213 <123782273+LucasZhu213@users.noreply.github.com>
2025-09-23 17:19:49 -0400

    Removed post-tsunami factors, applied Weka missing discretized certain vars that were interpreted as numeric

--- data/integrate_data.py
+++ data/integrate_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 
 df_runups = pd.read_csv("output_tsunami_runups.csv")
+df_runups_proc = pd.read_csv("output_tsunami_logtransformed_discretized_removeposttsunamifactors_discretizenumericids&cats.csv")
 
 col = 'runupHt'
 Q1 = df_runups[col].quantile(0.25)
@@ -11,6 +12,7 @@ lower_bound = Q1 - 1.5 * IQR
 upper_bound = Q3 + 1.5 * IQR
 
 df_runups = df_runups[(df_runups[col] >= lower_bound) & (df_runups[col] <= upper_bound)].copy()
+df_runups = df_runups.dropna(subset=[col])
 df_runups['logrunupHt'] = np.log(df_runups[col] + 1)
 count_nan = 0
 threshold = max(df_runups['logrunupHt'])/3
@@ -23,5 +25,9 @@ for index, instance in df_runups.iterrows():
         df_runups.loc[index, 'logrunupHt'] = "m"
     else: 
         df_runups.loc[index, 'logrunupHt'] = "l"
+
 df_runups = df_runups.drop("runupHt", axis=1)
-df_runups.to_csv("output_tsunami_logtransformed_discretized.csv", index=False)
+
+for col in df_runups.columns:
+    if col not in df_runups_proc.columns:
+        print(col)
